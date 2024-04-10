2daa62611a05ae9462779ae121776075c291b0b6
Robin <robin.vidal.pro@gmail.com>
2024-04-10 15:30:46 +0200

    User voice state detection & websocket comunication logic

--- autosoundboard.plugin.js
+++ autosoundboard.plugin.js
@@ -2,7 +2,7 @@
  * @name AutoSoundboard
  * @author rvhoney
  * @description Thank you for downloading this plugin. This extension allows you to automatically play soundboards when you join a Discord room. Please go to the settings panel to configure the extension. ⚠️ **A Stream Deck and an additional plugin are required for this plugin to work.**
- * @version 0.0.1
+ * @version 0.1.0
  * @authorId 770755946846355477
  * @source https://github.com/rvHoney/autosoundboard-betterdiscord
  */
@@ -13,6 +13,11 @@ module.exports = class AutoSoundboard {
      */
     start() {
         this.registerCSS();
+
+        const VoiceStateModule = BdApi.Webpack.getStore('VoiceStateStore');
+        const CurrentUserModule = BdApi.findModuleByProps("getCurrentUser");
+
+        this.isInVCLoop(VoiceStateModule, CurrentUserModule);
     }
 
     /**
@@ -22,12 +27,86 @@ module.exports = class AutoSoundboard {
     }
 
     /**
+     * Check if the user is in a voice channel.
+     * @param {Object} VoiceStateModule VoiceStateStore module.
+     * @param {Object} CurrentUserModule CurrentUser module.
+     * @returns {boolean} User's voice channel status.
+     */
+    isInVCLoop(VoiceStateModule, CurrentUserModule) {
+        const userId = CurrentUserModule.getCurrentUser().id;
+        const userVoiceState = VoiceStateModule.getVoiceStateForUser(userId);
+        if (userVoiceState) {
+            BdApi.saveData("autosoundboard", "isInVC", true);
+            if (this.loadSettings().isSoundAlreadyPlayed === false) {
+                this.playSound();
+                BdApi.saveData("autosoundboard", "isSoundAlreadyPlayed", true);
+            }
+        } else {
+            BdApi.saveData("autosoundboard", "isInVC", false);
+            BdApi.saveData("autosoundboard", "isSoundAlreadyPlayed", false);
+        }
+
+        setTimeout(() => {
+            this.isInVCLoop(VoiceStateModule, CurrentUserModule);
+        }, 500);
+    }
+
+    /** Send a message to the Stream Deck plugin to play a sound. */
+    playSound() {
+        const wsAddress = this.loadSettings().streamDeckWS;
+        const ws = new WebSocket(wsAddress);
+        ws.onopen = () => {
+            const data = {
+                type: "play sound"
+            }
+            ws.send(JSON.stringify(data));
+        }
+    }
+
+    /**
      * Retrieves plugin settings.
      * @returns {Object} Plugin settings.
      */
     loadSettings() {
         const streamDeckWS = BdApi.loadData("autosoundboard", "streamDeckWS");
-        return { streamDeckWS: streamDeckWS };
+        const isInVC = BdApi.loadData("autosoundboard", "isInVC");
+        const isSoundAlreadyPlayed = BdApi.loadData("autosoundboard", "isSoundAlreadyPlayed");
+        return { streamDeckWS: streamDeckWS, isInVC: isInVC, isSoundAlreadyPlayed: isSoundAlreadyPlayed };
+    }
+
+    /**
+     * Check connection to Stream Deck Plugin WebSocket.
+     * @param {string} wsAddress WebSocket address of the Stream Deck plugin.
+     * @returns {boolean} Connection status.
+     */
+    checkConnection(wsAddress) {
+        return new Promise((resolve, reject) => {
+            const ws = new WebSocket(wsAddress);
+            ws.onopen = () => {
+                const data = {
+                    type: "connection test"
+                }
+                ws.send(JSON.stringify(data));
+            }
+            ws.onmessage = (message) => {
+                message = JSON.parse(message.data);
+                if (message.type === "successful connection") {
+                    clearTimeout(timeout);
+                    resolve(true);
+                } else {
+                    clearTimeout(timeout);
+                    resolve(false);
+                }
+            }
+            ws.onerror = (error) => {
+                clearTimeout(timeout);
+                reject(error);
+            }
+    
+            const timeout = setTimeout(() => {
+                reject(new Error('Connection timeout'));
+            }, 5000); // 5 seconds timeout
+        });
     }
 
     /**
@@ -45,9 +124,6 @@ module.exports = class AutoSoundboard {
 
         const streamDeckWSDescription = document.createElement("p");
         streamDeckWSDescription.textContent = "Please enter the WebSocket address of your Stream Deck plugin. For more information, please refer to the documentation of the plugin to be installed on your Stream Deck.";
-
-        const streamDeckWSLinkSeparator = document.createElement("span");
-        streamDeckWSLinkSeparator.textContent = "|";
 
         const streamDeckWSError = document.createElement("p");
         streamDeckWSError.classList.add("autosb-ws-error");
@@ -93,6 +169,38 @@ module.exports = class AutoSoundboard {
         streamDeckWSLink2.target = "_blank";
         streamDeckWSLink2.textContent = "Stream Deck Plugin Repository";
 
+        const streamDeckWSLinkSeparator = document.createElement("span");
+        streamDeckWSLinkSeparator.textContent = "|";
+
+        const streamDeckWSTestConnectionContainer = document.createElement("div");
+        streamDeckWSTestConnectionContainer.classList.add("autosb-ws-test-connection");
+
+        const streamDeckWSTestConnection = document.createElement("button");
+        streamDeckWSTestConnection.textContent = "Test Connection";
+        streamDeckWSTestConnection.onclick = () => {
+            const wsAddress = this.loadSettings().streamDeckWS;
+            this.checkConnection(wsAddress)
+                .then(isConnected => {
+                    if (isConnected) {
+                        console.log('Connected successfully');
+                        streamDeckWSTestConnectionStatus.textContent = "Connection successful.";
+                        streamDeckWSTestConnectionStatus.style.color = "#2DC770";
+                        streamDeckWSTestConnectionStatus.style.display = "block";
+                    } else {
+                        streamDeckWSTestConnectionStatus.textContent = "Connection failed.";
+                        streamDeckWSTestConnectionStatus.style.color = "#FA777C";
+                        streamDeckWSTestConnectionStatus.style.display = "block";
+                    }
+                })
+                .catch(error => {
+                    streamDeckWSTestConnectionStatus.textContent = "Connection failed.";
+                    streamDeckWSTestConnectionStatus.style.color = "#FA777C";
+                    streamDeckWSTestConnectionStatus.style.display = "block";
+                });
+        };
+
+        const streamDeckWSTestConnectionStatus = document.createElement("p");
+
         streamDeckWSField.append(streamDeckWSDescription)
         streamDeckWSField.append(streamDeckWSError);
         streamDeckWSField.append(streamDeckWSSuccess);
@@ -102,6 +210,9 @@ module.exports = class AutoSoundboard {
         streamDeckWSLinkList.append(streamDeckWSLink1)
         streamDeckWSLinkList.append(streamDeckWSLinkSeparator);
         streamDeckWSLinkList.append(streamDeckWSLink2);
+        streamDeckWSField.append(streamDeckWSTestConnectionContainer);
+        streamDeckWSTestConnectionContainer.append(streamDeckWSTestConnection);
+        streamDeckWSTestConnectionContainer.append(streamDeckWSTestConnectionStatus);
         SettingsPanel.append(streamDeckWSField);
 
         return SettingsPanel;
@@ -172,12 +283,18 @@ module.exports = class AutoSoundboard {
                 margin-bottom: -5px;
             }
 
-            .autosb-ws-setting .autosb-ws-error,
+            .autosb-ws-setting .autosb-ws-error {
                 color: #FA777C;
             }
 
             .autosb-ws-setting .autosb-ws-success {
                 color: #2DC770;
+            }
+
+            .autosb-ws-test-connection {
+                display: flex;
+                gap: 10px;
+                margin-top: 15px;
             }
         `);
     }
